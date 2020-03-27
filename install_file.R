668cf12d84cb47a0018fed6dc08a120f9e3b074d
Barinder <barinder.thi@gmail.com>
2020-03-26 22:25:39 -0700

    installation file

---
+++ install_file.R
@@ -0,0 +1,6 @@
+# Initial File
+
+# Here, we install the appropriate packages
+install.packages(c("mvtnorm","loo","coda"), repos="https://cloud.r-project.org/",dependencies=TRUE)
+options(repos=c(getOption('repos'), rethinking='http://xcelab.net/R'))
+install.packages('rethinking',type='source')
